79f5ccaaf2a518d9806f2bc6e289a6a24ece45a6
Thomas van der Velde <contact@toolbarthomas.net>
2024-06-15 19:49:23 +0200

    update: Includes BPM timestep test

--- src/tests/default.test.ts
+++ src/tests/default.test.ts
@@ -1,20 +1,22 @@
-import assert from "assert";
+import assert, { equal } from "assert";
 import { describe, it } from "mocha";
 
 import { requestTimestep } from "src/index";
 
 describe("Can construct a new Timestep instance within the current process:", function () {
-  const maxDuration = 9000;
+  const maxDuration = 5000;
   this.timeout(maxDuration * 2);
 
   it("requestTimestep @30 FPS", async () =>
     new Promise<void>((resolve) => {
-      const targetFPS = 66;
+      const targetFPS = 30;
       let updateFPS = 0;
       let renderFPS = 0;
       let averageFPS = 0;
+      let frames = 0;
       const updateTimestamps: number[] = [];
       const renderTimestamps: number[] = [];
+      const expectedFPS = (maxDuration / 2 / 1000) * targetFPS;
 
       const timestep = requestTimestep({
         onUpdate: function (response) {
@@ -32,6 +34,7 @@ describe("Can construct a new Timestep instance within the current process:", fu
           renderTimestamps.push(response.timestamp);
           renderFPS = 1000 / response.delta;
           averageFPS = response.averageFPS;
+          frames += 1;
 
           // Accept a 50% framedrop tolerance;
           assert.equal(
@@ -62,10 +65,38 @@ describe("Can construct a new Timestep instance within the current process:", fu
         assert.equal(renderFPS > 0, true);
         assert.equal(averageFPS > 0, true);
         assert.equal(averageFPS >= targetFPS - targetFPS * 0.1, true);
-
-        console.log(averageFPS);
+        assert.equal(
+          frames >= expectedFPS * 0.9 && frames <= expectedFPS * 1.1,
+          true
+        );
 
         resolve();
-      }, maxDuration);
+      }, maxDuration / 2);
+    }));
+
+  it("requestTimestamp: 128 BPM", async () =>
+    new Promise<void>((resolve) => {
+      const BPM = 128;
+      const targetFPS = BPM / 60;
+      const start = performance.now();
+      let beats = 0;
+
+      const track = requestTimestep({
+        onRender: function (response) {
+          beats += 1;
+        },
+        fps: targetFPS,
+      });
+
+      setTimeout(() => {
+        assert.equal(
+          beats,
+          Math.floor(((performance.now() - start) * targetFPS) / 1000)
+        );
+
+        track.stop();
+
+        resolve();
+      }, 3000);
     }));
 });
